eb9b8e204908de7d925023d6d2c70ed050901a6f
AndrewShidel <andrew@shidel.com>
2015-01-23 23:08:54 -0500

    moved android app to a subdirectory

---
+++ android/app/src/main/java/drone/shidel/andrew/myapplication/Controller.java
@@ -0,0 +1,17 @@
+package drone.shidel.andrew.myapplication;
+
+/**
+ * Created by andrew on 1/1/15.
+ */
+public class Controller {
+    private static final String serverAddress = "127.0.0.1:9090";
+    public void connect(){
+
+    }
+    public String getCommand(){
+        return null;
+    }
+    public void sendToClient(String message){
+
+    }
+}
