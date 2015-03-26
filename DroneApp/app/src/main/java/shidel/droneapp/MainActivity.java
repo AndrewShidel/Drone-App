1864da6b26e27ed4fc5e9acfe54d67602bc1f146
AndrewShidel <andrew@shidel.com>
2015-03-26 16:09:53 -0400

    MainActivity now utilizes new motor and usb protocals

--- DroneApp/app/src/main/java/shidel/droneapp/MainActivity.java
+++ DroneApp/app/src/main/java/shidel/droneapp/MainActivity.java
@@ -1,6 +1,10 @@
 package shidel.droneapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,8 +13,11 @@ import android.widget.TextView;
 
 import java.io.IOException;
 
+import shidel.droneapp.USBInterface.MaestroSSC;
+
 
 public class MainActivity extends Activity {
+    private MaestroSSC maestroSSC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -24,6 +31,38 @@ public class MainActivity extends Activity {
             loop.start(null);
         } catch(IOException e) {
             ((TextView)this.findViewById(R.id.debugX)).setText("Could not connect the server.");
+        }
+        maestroSSC = new MaestroSSC();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+//		Log.d(TAG, "onResume(" + intent + ")");
+        String action = intent.getAction();
+
+        if (action.equals("android.hardware.usb.action.USB_DEVICE_ATTACHED")) {
+            UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+            if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+                UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+                maestroSSC.setDevice(usbManager, device);
+
+                Motors motors = new Motors(maestroSSC);
+                motors.armEscs(new Motors.MotorTaskCallback() {
+                    @Override
+                    public void finished() {
+                        try {
+                            new MainLoop(MainActivity.this).start(new Controller());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                maestroSSC.setDevice(null, null);
+            } else {
+                Log.d("debug", "Unexpected Action=" + action.toString());
+            }
         }
     }
 }
